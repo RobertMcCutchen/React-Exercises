817808c055e2215e95a16f0da68a100fc363cc85
Robert McCutchen <robertmccutchen@Roberts-MacBook-Air.local>
2019-09-08 21:00:43 -0500

    React Exercises

---
+++ react-hooks-videoplayer-start/src/components/PlaylistItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PlaylistItems = props => {
+   <div>
+      PlaylistItems!
+   </div>   
+}
+
+export default PlaylistItems;
---
+++ react-hooks-videoplayer-start/src/components/containers/WbnPlayer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import video from '../video';
+import Playlist from '../containers/Playlist';
+
+const WbnPlayer = props => {
+    return(
+        <>
+            <Video />
+            <Playlist />
+        </>
+    )    
+}
+
+export default WbnPlayer
